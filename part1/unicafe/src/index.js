29bc2cde3bb219fcb67a43fb550cf316fad1dc97
Ilja Smelich <ismelich@c2r6p7.hive.fi>
2020-09-09 16:54:25 +0300

    bonus task anecdote done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -25,18 +25,22 @@ const Statistics = ({ good, neutral, bad }) => {
         <Statistic text="good" value={good} />
         <Statistic text="neutral" value={neutral} />
         <Statistic text="bad" value={bad} />
+        <Statistic text="all" value={all} />
         <Statistic text="average" value={average} />
         <Statistic text="positive" value={positive} percent=" %" />
       </tbody>
     </table>
   );
-  const noFeedback = <div>No feedback given</div>;
+  const noFeedback = (
+    <div>
+      <p>No feedback given</p>
+    </div>
+  );
 
   return all === 0 ? noFeedback : feedback;
 };
 
 const App = () => {
-  // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -44,10 +48,6 @@ const App = () => {
   const increaseGood = () => setGood(good + 1);
   const increaseNeutral = () => setNeutral(neutral + 1);
   const increaseBad = () => setBad(bad + 1);
-
-  console.log(good);
-  console.log(neutral);
-  console.log(bad);
 
   return (
     <div>
