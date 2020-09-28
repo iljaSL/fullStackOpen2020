c701d61bbeb113a286d937c07762a6ee2030017d
Ilja Smelich <iljasmelich@gmail.com>
2020-09-28 17:18:10 +0300

    started with part 4C

--- part4/blogList/models/blog.js
+++ part4/blogList/models/blog.js
@@ -8,6 +8,7 @@ const blogSchema = mongoose.Schema({
 	author: String,
 	url: { type: String, required: true },
 	likes: { type: Number, default: 0 },
+	user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 });
 
 blogSchema.plugin(uniqueValidator);
