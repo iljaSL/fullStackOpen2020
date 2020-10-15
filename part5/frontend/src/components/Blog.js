8b69dafc4a99331f09a4ff98023f60e5df2c34be
Ilja Smelich <iljasmelich@gmail.com>
2020-10-15 11:52:20 +0300

    ex 5.7 done

--- part5/frontend/src/components/Blog.js
+++ part5/frontend/src/components/Blog.js
@@ -6,22 +6,35 @@ const Blog = ({ blog }) => {
 		setVisibility(!visibility);
 	};
 
+	const blogStyle = {
+		paddingTop: 10,
+		paddingLeft: 2,
+		border: 'solid',
+		width: 270,
+		borderWidth: 1,
+		marginBottom: 5,
+	};
+
 	return (
-		<>
-			{blog.title} {blog.author}{' '}
-			<button onClick={toggleVisibility}>{visibility ? 'Hide' : 'View'}</button>
+		<div style={blogStyle}>
+			{blog.title} by {blog.author} {console.log(blog.user.name)}
+			<button onClick={toggleVisibility}>{visibility ? 'hide' : 'view'}</button>
 			{visibility && (
 				<div>
 					<p>
-						link:{' '}
+						link:
 						<a href={blog.url} target='_blank' rel='noopener noreferrer'>
 							{blog.url}
 						</a>
 					</p>
+					<p>
+						likes 0 <button>like</button>
+					</p>
+					<p>posted by {blog.user.name}</p>
 				</div>
 			)}
 			<br />
-		</>
+		</div>
 	);
 };
 
