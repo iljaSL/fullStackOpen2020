9e3defa200b199ddea1ef2269c4399d8ee4585b3
Ilja Smelich <iljasmelich@gmail.com>
2020-10-15 16:40:46 +0300

    ex 5.11 and 5.12 done

--- part5/frontend/src/App.js
+++ part5/frontend/src/App.js
@@ -4,34 +4,34 @@ import LoginForm from './components/LoginForm';
 import Blogs from './components/Blogs';
 
 const App = () => {
-	const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
-	const [notification, setNotification] = useState(null);
-	const [errorMessage, setErrorMessage] = useState(null);
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
+  const [notification, setNotification] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-	return (
-		<div>
-			<Notification
-				message={notification}
-				type={errorMessage}
-				setNotification={setNotification}
-				setErrorMessage={setErrorMessage}
-			/>
-			{user ? (
-				<Blogs
-					user={user}
-					setUser={setUser}
-					setNotification={setNotification}
-					setErrorMessage={setErrorMessage}
-				/>
-			) : (
-				<LoginForm
-					setUser={setUser}
-					setNotification={setNotification}
-					setErrorMessage={setErrorMessage}
-				/>
-			)}
-		</div>
-	);
+  return (
+    <div>
+      <Notification
+        message={notification}
+        type={errorMessage}
+        setNotification={setNotification}
+        setErrorMessage={setErrorMessage}
+      />
+      {user ? (
+        <Blogs
+          user={user}
+          setUser={setUser}
+          setNotification={setNotification}
+          setErrorMessage={setErrorMessage}
+        />
+      ) : (
+        <LoginForm
+          setUser={setUser}
+          setNotification={setNotification}
+          setErrorMessage={setErrorMessage}
+        />
+      )}
+    </div>
+  );
 };
 
 export default App;
--- part5/frontend/src/components/Notification.js
+++ part5/frontend/src/components/Notification.js
@@ -1,48 +1,48 @@
 import React, { useEffect } from 'react';
 
 const Notification = ({ message, type, setNotification, setErrorMessage }) => {
-	useEffect(() => {
-		if (message) {
-			const timer = setTimeout(() => {
-				setNotification('');
-				setErrorMessage('');
-			}, 5000);
-			return () => clearTimeout(timer);
-		}
-		// eslint-disable-next-line
-	}, [message]);
+  useEffect(() => {
+    if (message) {
+      const timer = setTimeout(() => {
+        setNotification('');
+        setErrorMessage('');
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+    // eslint-disable-next-line
+  }, [message]);
 
-	const successNotification = {
-		background: '#DFF2BF',
-		color: '#4F8A10',
-		margin: '10px 0px',
-		padding: '12px',
-	};
+  const successNotification = {
+    background: '#DFF2BF',
+    color: '#4F8A10',
+    margin: '10px 0px',
+    padding: '12px',
+  };
 
-	const updateNotification = {
-		background: '#BDE5F8',
-		color: '#00529B',
-		margin: '10px 0px',
-		padding: '12px',
-	};
+  const updateNotification = {
+    background: '#BDE5F8',
+    color: '#00529B',
+    margin: '10px 0px',
+    padding: '12px',
+  };
 
-	const errorNotification = {
-		background: '#FFD2D2',
-		color: '#D8000C',
-		margin: '10px 0px',
-		padding: '12px',
-	};
+  const errorNotification = {
+    background: '#FFD2D2',
+    color: '#D8000C',
+    margin: '10px 0px',
+    padding: '12px',
+  };
 
-	const notificationStyle =
-		message === null
-			? null
-			: type === 'ok'
-			? successNotification
-			: type === 'update'
-			? updateNotification
-			: errorNotification;
+  const notificationStyle =
+    message === null
+      ? null
+      : type === 'ok'
+        ? successNotification
+        : type === 'update'
+          ? updateNotification
+          : errorNotification;
 
-	return <div style={notificationStyle}>{message}</div>;
+  return <div style={notificationStyle}>{message}</div>;
 };
 
 export default Notification;
