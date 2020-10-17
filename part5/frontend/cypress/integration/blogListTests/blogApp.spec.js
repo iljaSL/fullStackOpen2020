ed33586c5f48f7e5e73191b0c14bc547f9980f9a
Ilja Smelich <iljasmelich@gmail.com>
2020-10-17 19:02:11 +0300

    ex 5.22 done

--- part5/frontend/cypress/integration/blogListTests/blogApp.spec.js
+++ part5/frontend/cypress/integration/blogListTests/blogApp.spec.js
@@ -1,112 +1,129 @@
-const { func } = require("prop-types");
+const { func } = require('prop-types');
 
 describe('checking that the application displays the login form by default.', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset');
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: '12345'
+    };
+    cy.request('POST', 'http://localhost:3001/api/users/', user);
+    cy.visit('http://localhost:3000');
+  });
+
+  it('Login form is shown', function () {
+    cy.contains('log in to the application');
+    cy.get('#login-form');
+    cy.get('#username');
+    cy.get('#password');
+    cy.get('#login-button');
+  });
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('mluukkai');
+      cy.get('#password').type('12345');
+      cy.get('#login-button').click();
+      cy.contains('Matti Luukkainen logged in');
+    });
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('mluukkai');
+      cy.get('#password').type('1234');
+      cy.get('#login-button').click();
+      cy.get('.error')
+        .should('contain', 'wrong username or password')
+        .should('have.css', 'background-color', 'rgb(255, 210, 210)');
+    });
+  });
+
+  describe('When logged in', function () {
     beforeEach(function () {
-        cy.request('POST', 'http://localhost:3001/api/testing/reset');
-        const user = {
-            name: 'Matti Luukkainen',
-            username: 'mluukkai',
-            password: '12345'
-        }
-        cy.request('POST', 'http://localhost:3001/api/users/', user)
-        cy.visit('http://localhost:3000');
-    })
+      cy.get('#username').type('mluukkai');
+      cy.get('#password').type('12345');
+      cy.get('#login-button').click();
+    });
 
-    it('Login form is shown', function () {
-        cy.contains('log in to the application');
-        cy.get('#login-form');
-        cy.get('#username');
-        cy.get('#password');
-        cy.get('#login-button');
-    })
+    it('A blog can be created', function () {
+      cy.contains('Create new blog').click();
+      cy.get('#title').type('Cool Post 2');
+      cy.get('#author').type('Peter Lustig');
+      cy.get('#url').type('https://test.com');
+      cy.get('#create-blog-button').click();
+      cy.get('#blogs > *').should('have.length', 1);
+      cy.contains('Cool Post 2');
+      cy.contains('Peter Lustig');
+    });
 
-    describe('Login', function () {
-        it('succeeds with correct credentials', function () {
-            cy.get('#username').type('mluukkai');
-            cy.get('#password').type('12345');
-            cy.get('#login-button').click();
-            cy.contains('Matti Luukkainen logged in');
-        })
+    it('checks that user can like a blog', function () {
+      cy.contains('Create new blog').click();
+      cy.get('#title').type('Cool Post 2');
+      cy.get('#author').type('Peter Lustig');
+      cy.get('#url').type('https://test.com');
+      cy.get('#create-blog-button').click();
+      cy.get('.toggle-button').click();
+      cy.contains('likes 0');
+      cy.get('.like-button').click();
+      cy.contains('likes 1');
+    });
+  });
 
-        it('fails with wrong credentials', function () {
-            cy.get('#username').type('mluukkai');
-            cy.get('#password').type('1234');
-            cy.get('#login-button').click();
-            cy.get('.error')
-                .should('contain', 'wrong username or password')
-                .should('have.css', 'background-color', 'rgb(255, 210, 210)');
-        })
-    })
+  describe('test with user id from local stroge', function () {
+    beforeEach(function () {
+      const user = {
+        username: 'plustig',
+        password: '12345',
+        name: 'Peter Lustig'
+      };
+      cy.createUser(user);
+      cy.login(user);
 
-    describe('When logged in', function () {
-        beforeEach(function () {
-            cy.get('#username').type('mluukkai');
-            cy.get('#password').type('12345');
-            cy.get('#login-button').click();
-        })
+      const blog = {
+        title: 'Cool Post 3',
+        author: 'Peter Lustig',
+        url: 'https://test.com',
+        likes: 10,
+      };
+      cy.createBlogPost(blog);
+    });
 
-        it('A blog can be created', function () {
-            cy.contains('Create new blog').click();
-            cy.get('#title').type('Cool Post 2');
-            cy.get('#author').type('Peter Lustig');
-            cy.get('#url').type('https://test.com');
-            cy.get('#create-blog-button').click();
-            cy.get('#blogs > *').should('have.length', 1);
-            cy.contains('Cool Post 2');
-            cy.contains('Peter Lustig');
-        })
+    it('user can delete own post', function () {
+      cy.get('#blogs > *').should('have.length', 1);
+      cy.get('.toggle-button').click();
+      cy.get('#remove-button').click();
+      cy.get('#blogs > *').should('have.length', 0);
+    });
 
-        it('checks that user can like a blog', function () {
-            cy.contains('Create new blog').click();
-            cy.get('#title').type('Cool Post 2');
-            cy.get('#author').type('Peter Lustig');
-            cy.get('#url').type('https://test.com');
-            cy.get('#create-blog-button').click();
-            cy.get('.toggle-button').click();
-            cy.contains('likes 0');
-            cy.get('.like-button').click();
-            cy.contains('likes 1');
-        })
-    })
+    it('user can not delete other user post', function () {
+      cy.get('#logout-button').click();
 
-    describe('test with user id from local stroge', function () {
-        beforeEach(function () {
-            const user = {
-                username: 'plustig',
-                password: '12345',
-                name: 'Peter Lustig'
-            };
-            cy.createUser(user);
-            cy.login(user);
+      const user = {
+        username: 'pmueller',
+        password: '12345',
+        name: 'Peter Mueller'
+      };
 
-            const blog = {
-                title: 'Cool Post 3',
-                author: 'Peter Lustig',
-                url: 'https://test.com'
-            };
-            cy.createBlogPost(blog);
-        });
+      cy.createUser(user);
+      cy.login(user);
+      cy.get('.toggle-button').click();
+      cy.get('#remove-button').should('not.exist');
+    });
 
-        it('user can delete own post', function () {
-            cy.get('#blogs > *').should('have.length', 1);
-            cy.get('.toggle-button').click();
-            cy.get('#remove-button').click();
-            cy.get('#blogs > *').should('have.length', 0);
-        })
-
-        it('user can not delete other user post', function () {
-            cy.get('#logout-button').click();
-
-            const user = {
-                username: 'pmueller',
-                password: '12345',
-                name: 'Peter Mueller'
-            };
-
-            cy.createUser(user);
-            cy.login(user);
-            cy.get('.toggle-button').click();
-            cy.get('#remove-button').should('not.exist');
-        })
-    })
-})
+    it('blogs are ordered according to likes with the blog with the most likes being first.', function () {
+      cy.createBlogPost({
+        title: 'Cool Post 4',
+        author: 'Peter Lustig',
+        url: 'https://test.com',
+        likes: 20,
+      });
+      cy.createBlogPost({
+        title: 'Cool Post 5',
+        author: 'Peter Lustig',
+        url: 'https://test.com',
+        likes: 15,
+      });
+      cy.get('#blogs').first().contains('Cool Post 5');
+    });
+  });
+});
--- part5/frontend/src/tests/BlogForm.test.js
+++ part5/frontend/src/tests/BlogForm.test.js
@@ -4,27 +4,27 @@ import BlogForm from '../components/BlogForm';
 import { render, fireEvent } from '@testing-library/react';
 
 describe('testing the BlogForm component', () => {
-    test('check, that the form calls the event handler it received as props with the right details when a new blog is created.', () => {
-        const { container } = render(<BlogForm />);
+  test('check, that the form calls the event handler it received as props with the right details when a new blog is created.', () => {
+    const { container } = render(<BlogForm />);
 
-        const title = container.querySelector('#title');
-        const author = container.querySelector('#author');
-        const url = container.querySelector('#url');
+    const title = container.querySelector('#title');
+    const author = container.querySelector('#author');
+    const url = container.querySelector('#url');
 
-        fireEvent.change(title, {
-            target: { value: 'Cool Post 5' }
-        });
+    fireEvent.change(title, {
+      target: { value: 'Cool Post 5' }
+    });
 
-        fireEvent.change(author, {
-            target: { value: 'Peter Lustig' }
-        });
+    fireEvent.change(author, {
+      target: { value: 'Peter Lustig' }
+    });
 
-        fireEvent.change(url, {
-            target: { value: 'https://test.com' }
-        });
+    fireEvent.change(url, {
+      target: { value: 'https://test.com' }
+    });
 
-        expect(title.value).toBe('Cool Post 5');
-        expect(author.value).toBe('Peter Lustig');
-        expect(url.value).toBe('https://test.com');
-    })
-})
+    expect(title.value).toBe('Cool Post 5');
+    expect(author.value).toBe('Peter Lustig');
+    expect(url.value).toBe('https://test.com');
+  });
+});
