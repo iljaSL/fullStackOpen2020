fbfbc490aa3eec1e6f360b6da5c8acf8bc8d53d4
Ilja Smelich <iljasmelich@gmail.com>
2020-10-19 16:39:34 +0300

    finished ex6.4

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,39 +1,44 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { voteForAnecdotes } from './reducers/anecdoteReducer'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { voteForAnecdotes } from './reducers/anecdoteReducer';
+import { createAnecdote } from './reducers/anecdoteReducer';
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+	const anecdotes = useSelector((state) => state);
+	const dispatch = useDispatch();
 
-  const vote = (id) => {
-    dispatch(voteForAnecdotes(id))
-  }
+	const vote = (id) => {
+		dispatch(voteForAnecdotes(id));
+	};
 
-  return (
-    <div>
-      <h2>Anecdotes</h2>
-      {
-        []
-          .concat(anecdotes)
-          .map(anecdote =>
-            <div key={anecdote.id}>
-              <div>
-                {anecdote.content}
-              </div>
-              <div>
-                has {anecdote.votes}
-                <button onClick={() => vote(anecdote.id)}>vote</button>
-              </div>
-            </div>
-          )}
-      <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
-      </form>
-    </div>
-  )
-}
+	const create = (e) => {
+		e.preventDefault();
+		const content = e.target.anecdote.value;
+		dispatch(createAnecdote(content));
+		e.target.anecdote.value = '';
+	};
 
-export default App
+	return (
+		<div>
+			<h2>Anecdotes</h2>
+			{[].concat(anecdotes).map((anecdote) => (
+				<div key={anecdote.id}>
+					<div>{anecdote.content}</div>
+					<div>
+						has {anecdote.votes}
+						<button onClick={() => vote(anecdote.id)}>vote</button>
+					</div>
+				</div>
+			))}
+			<h2>create new</h2>
+			<form onSubmit={create}>
+				<div>
+					<input name='anecdote' />
+				</div>
+				<button>create</button>
+			</form>
+		</div>
+	);
+};
+
+export default App;
